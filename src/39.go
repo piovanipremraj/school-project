a159113d749a79b39952b7060cacae0274203ba2
piovanipremraj <piovanipremraj@gmail.com>
2025-05-25 16:35:54 +0000

    Add 39.go

---
+++ src/39.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x int = 5
+    if x > 10 {
+        fmt.Println("x is greater than 10")
+    } else {
+        fmt.Println("x is not greater than 10")
+    }
+}
